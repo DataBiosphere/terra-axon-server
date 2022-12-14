ea8a2271f3bd3fd7ce84ea3959c768de4452a8f0
rogerwangcs <rogerwangcs@gmail.com>
2022-12-14 10:49:42 -0500

    remove sam

--- service/src/main/java/bio/terra/axonserver/app/configuration/SamConfiguration.java
+++
@@ -1,6 +0,0 @@
-package bio.terra.axonserver.app.configuration;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "axonserver.sam")
-public record SamConfiguration(String basePath, String resourceId) {}
