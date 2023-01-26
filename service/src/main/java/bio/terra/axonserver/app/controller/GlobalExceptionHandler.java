f2a2a0e4faa8e84c14bded0e03467600f451252d
Aaron Wang <66176554+aaron-wang@users.noreply.github.com>
2023-01-26 19:59:50 +0000

    Add 'api' prefix back

--- service/src/main/java/bio/terra/axonserver/app/controller/GlobalExceptionHandler.java
+++ service/src/main/java/bio/terra/axonserver/app/controller/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package bio.terra.axonserver.app.controller;
 
-import bio.terra.axonserver.model.ErrorReport;
+import bio.terra.axonserver.model.ApiErrorReport;
 import bio.terra.common.exception.AbstractGlobalExceptionHandler;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends AbstractGlobalExceptionHandler<ErrorReport> {
+public class GlobalExceptionHandler extends AbstractGlobalExceptionHandler<ApiErrorReport> {
 
   @Override
-  public ErrorReport generateErrorReport(Throwable ex, HttpStatus statusCode, List<String> causes) {
-    return new ErrorReport().message(ex.getMessage()).statusCode(statusCode.value()).causes(causes);
+  public ApiErrorReport generateErrorReport(
+      Throwable ex, HttpStatus statusCode, List<String> causes) {
+    return new ApiErrorReport()
+        .message(ex.getMessage())
+        .statusCode(statusCode.value())
+        .causes(causes);
   }
 }
