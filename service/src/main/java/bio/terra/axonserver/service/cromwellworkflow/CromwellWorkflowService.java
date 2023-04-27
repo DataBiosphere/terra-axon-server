7751b56ba36ea853c68918089a4e1b18f9a9118e
Aaron Wang <66176554+aaron-wang@users.noreply.github.com>
2023-04-27 19:15:09 +0000

    Rename.

--- service/src/main/java/bio/terra/axonserver/service/cromwellworkflow/CromwellWorkflowService.java
+++ service/src/main/java/bio/terra/axonserver/service/cromwellworkflow/CromwellWorkflowService.java
@@ -2,7 +2,7 @@ package bio.terra.axonserver.service.cromwellworkflow;
 
 import bio.terra.axonserver.app.configuration.CromwellConfiguration;
 import bio.terra.axonserver.model.ApiCallMetadata;
-import bio.terra.axonserver.model.ApiFailureMessage;
+import bio.terra.axonserver.model.ApiFailureMessages;
 import bio.terra.axonserver.model.ApiWorkflowMetadataResponse;
 import bio.terra.axonserver.model.ApiWorkflowMetadataResponseSubmittedFiles;
 import bio.terra.axonserver.model.ApiWorkflowQueryResponse;
@@ -13,7 +13,7 @@ import bio.terra.cromwell.api.WorkflowsApi;
 import bio.terra.cromwell.client.ApiClient;
 import bio.terra.cromwell.client.ApiException;
 import io.swagger.client.model.CromwellApiCallMetadata;
-import io.swagger.client.model.CromwellApiFailureMessage;
+import io.swagger.client.model.CromwellApiFailureMessages;
 import io.swagger.client.model.CromwellApiLabelsResponse;
 import io.swagger.client.model.CromwellApiWorkflowIdAndStatus;
 import io.swagger.client.model.CromwellApiWorkflowMetadataResponse;
@@ -216,7 +216,7 @@ public class CromwellWorkflowService {
                                             .start(m.getStart())
                                             .end(m.getEnd())
                                             .jobId(m.getJobId())
-                                            .failures(toApiFailureMessageList(m.getFailures()))
+                                            .failures(toApiFailureMessages(m.getFailures()))
                                             .returnCode(m.getReturnCode())
                                             .callRoot(m.getCallRoot())
                                             .stdout(m.getStdout())
@@ -249,20 +249,20 @@ public class CromwellWorkflowService {
         .outputs(metadataResponse.getOutputs())
         .submittedFiles(submittedFiles)
         .calls(callMetadataMap)
-        .failures(toApiFailureMessageList(metadataResponse.getFailures()));
+        .failures(toApiFailureMessages(metadataResponse.getFailures()));
   }
 
-  private static List<ApiFailureMessage> toApiFailureMessageList(
-      List<CromwellApiFailureMessage> failureMessage) {
-    if (failureMessage == null) {
+  private static List<ApiFailureMessages> toApiFailureMessages(
+      List<CromwellApiFailureMessages> failureMessages) {
+    if (failureMessages == null) {
       return null;
     }
-    return failureMessage.stream()
+    return failureMessages.stream()
         .map(
             failure ->
-                new ApiFailureMessage()
+                new ApiFailureMessages()
                     .message(failure.getMessage())
-                    .causedBy(toApiFailureMessageList(failure.getCausedBy())))
+                    .causedBy(toApiFailureMessages(failure.getCausedBy())))
         .toList();
   }
 }
