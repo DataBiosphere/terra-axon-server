89a046d5776787ec6e3a61e0af8ec7963a1fc5c2
Dex Amundsen <dexamundsen@verily.com>
2023-08-31 21:40:13 -0700

    [BENCH-1053] remove aws-enabled check on resource access

--- service/src/main/java/bio/terra/axonserver/service/exception/FeatureNotEnabledException.java
+++ service/src/main/java/bio/terra/axonserver/service/exception/FeatureNotEnabledException.java
@@ -2,6 +2,8 @@ package bio.terra.axonserver.service.exception;
 
 import bio.terra.common.exception.NotImplementedException;
 
+// TODO(BENCH-1050) use FeatureNotSupportedException from TCL
+
 public class FeatureNotEnabledException extends NotImplementedException {
   public FeatureNotEnabledException(String message) {
     super(message);
