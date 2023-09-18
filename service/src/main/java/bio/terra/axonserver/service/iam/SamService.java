d3516294c1d35d36551d1f2f78f78d62ddfdcd36
Harris Mendell <harrismendell@gmail.com>
2023-09-18 16:02:56 -0400

    logic to initialize AFS in SAM (#61)

--- service/src/main/java/bio/terra/axonserver/service/iam/SamService.java
+++ service/src/main/java/bio/terra/axonserver/service/iam/SamService.java
@@ -4,6 +4,7 @@ import bio.terra.axonserver.app.configuration.SamConfiguration;
 import bio.terra.axonserver.service.cloud.gcp.GcpService;
 import bio.terra.common.exception.InternalServerErrorException;
 import bio.terra.common.iam.BearerToken;
+import bio.terra.common.sam.SamRetry;
 import bio.terra.common.sam.exception.SamExceptionFactory;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.common.collect.ImmutableSet;
@@ -14,14 +15,18 @@ import org.broadinstitute.dsde.workbench.client.sam.ApiException;
 import org.broadinstitute.dsde.workbench.client.sam.api.GoogleApi;
 import org.broadinstitute.dsde.workbench.client.sam.api.UsersApi;
 import org.broadinstitute.dsde.workbench.client.sam.model.UserStatusInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SamService {
-
+  private static final Logger logger = LoggerFactory.getLogger(SamService.class);
   private static final Set<String> SAM_OAUTH_SCOPES = ImmutableSet.of("openid", "email", "profile");
   private final SamConfiguration samConfig;
+  private boolean axonServiceAccountInitialized;
 
   @Autowired
   public SamService(SamConfiguration samConfig) {
@@ -85,11 +90,66 @@ public class SamService {
 
   public String getPetServiceAccountKey(String projectId, String userEmail) {
     try {
+      initializeAxonServiceAccount();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    try {
       return new GoogleApi(getApiClient(getAxonServiceAccountToken()))
           .getUserPetServiceAccountKey(projectId, userEmail);
     } catch (ApiException apiException) {
       throw SamExceptionFactory.create(
           "Error getting user's pet SA key for project.", apiException);
+    }
+  }
+
+  private void initializeAxonServiceAccount() throws InterruptedException {
+    if (!axonServiceAccountInitialized) {
+      final String axonAccessToken;
+      try {
+        axonAccessToken = getAxonServiceAccountToken();
+      } catch (InternalServerErrorException e) {
+        // In cases where Axon is not running as a service account (e.g. unit tests), the above call
+        // will throw. This can be ignored now and later when the credentials are used again.
+        logger.warn(
+            "Failed to register Axon service account in Sam. This is expected for tests.", e);
+        return;
+      }
+      UsersApi usersApi = new UsersApi(getApiClient(axonAccessToken));
+      // If registering the service account fails, all we can do is to keep trying.
+      if (!axonServiceAccountRegistered(usersApi)) {
+        // retries internally
+        registerAxonServiceAccount(usersApi);
+      }
+      axonServiceAccountInitialized = true;
+    }
+  }
+
+  public boolean axonServiceAccountRegistered(UsersApi usersApi) throws InterruptedException {
+    try {
+      // getUserStatusInfo throws a 404 if the calling user is not registered, which will happen
+      // the first time Axon is run in each environment.
+      SamRetry.retry(usersApi::getUserStatusInfo);
+      logger.info("Axon service account already registered in Sam");
+      return true;
+    } catch (ApiException apiException) {
+      if (apiException.getCode() == HttpStatus.NOT_FOUND.value()) {
+        logger.info(
+            "Sam error was NOT_FOUND when checking user registration. This means the "
+                + " user is not registered but is not an exception. Returning false.");
+        return false;
+      } else {
+        throw SamExceptionFactory.create("Error checking user status in Sam", apiException);
+      }
+    }
+  }
+
+  private void registerAxonServiceAccount(UsersApi usersApi) throws InterruptedException {
+    try {
+      SamRetry.retry(() -> usersApi.createUserV2(""));
+    } catch (ApiException apiException) {
+      throw SamExceptionFactory.create(
+          "Error registering Axon service account with Sam", apiException);
     }
   }
 
