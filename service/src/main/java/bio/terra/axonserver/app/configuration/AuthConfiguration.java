181970a12a3f4c1d583c80d729cc24fbe34ece58
cbookg <cbook@google.com>
2023-09-28 19:32:07 +0000

    Remove unused auth endpoints. (#66)
    We no longer support google login

--- service/src/main/java/bio/terra/axonserver/app/configuration/AuthConfiguration.java
+++
@@ -1,6 +0,0 @@
-package bio.terra.axonserver.app.configuration;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "axonserver.auth")
-public record AuthConfiguration(String clientId, String clientSecret) {}
